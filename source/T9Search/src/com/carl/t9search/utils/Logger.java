cfb58326bbec65a754eb84ef23cda07d8b0b0c80
Carl Li <huabeiyipilang@gmail.com>
2014-12-28 16:03:01 +0800

    正式版屏蔽日志

--- source/T9Search/src/com/carl/t9search/utils/Logger.java
+++ source/T9Search/src/com/carl/t9search/utils/Logger.java
@@ -1,5 +1,7 @@
 package com.carl.t9search.utils;
 
+import com.carl.t9search.BuildConfig;
+
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -21,18 +23,22 @@ public class Logger {
 	}
 	
 	public static void info(String msg){
+		if(BuildConfig.DEBUG)
 		Log.i(TAG, msg);
 	}
 	
 	public static void error(String msg){
+		if(BuildConfig.DEBUG)
 		Log.e(TAG, msg);
 	}
 	
 	public void i(String msg){
+		if(BuildConfig.DEBUG)
 		Log.i(TAG, buildMessage(msg));
 	}
 	
 	public void e(String msg){
+		if(BuildConfig.DEBUG)
 		Log.e(TAG, buildMessage(msg));
 	}
 	
